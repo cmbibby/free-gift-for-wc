1f3b3ca565f39cf611ac3765919abe83f09868dd
Chris Bibby <cmbibby@gmail.com>
2020-11-27 18:56:14 +0800

    fix typo

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -7,11 +7,10 @@ class Plugin {
 	public function __construct() {
 		 new Settings;
 		// check to see whether the user is allowed a free gift
-
-		$user_id       = \get_current_user_id();
-		$user          = new \WP_User( $user_id );
-		$exluded_roles = Settings::get_excluded_roles();
-		if ( ! is_array( $excluded_roles ) || empty( array_intersect( $user->roles, Settings::get_excluded_roles() ) ) ) {
+		$user_id        = \get_current_user_id();
+		$user           = new \WP_User( $user_id );
+		$excluded_roles = Settings::get_excluded_roles();
+		if ( ! is_array( $excluded_roles ) || empty( array_intersect( $user->roles, $excluded_roles ) ) ) {
 			new Free_Gift;
 		}
 	}
